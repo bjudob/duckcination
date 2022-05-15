4aad089159622dd82d47b2d2ebd41aabc281fbdc
Koncz Botond <konczbotondvagyok@gmail.com>
2022-05-15 15:34:15 +0300

    docs: remove comments

--- lib/ui/screens/child_screen.dart
+++ lib/ui/screens/child_screen.dart
@@ -2,7 +2,6 @@ import 'package:duckcination/models/child.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../../models/vaccine.dart';
 import '../elements/vaccine_card.dart';
 
 class ChildScreen extends StatefulWidget {
@@ -19,13 +18,9 @@ class _ChildScreenState extends State<ChildScreen> {
     return Scaffold(
       backgroundColor: Colors.amberAccent.shade100,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Duckcination'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: SingleChildScrollView(
             child: Column(
           children: [
@@ -44,25 +39,3 @@ class _ChildScreenState extends State<ChildScreen> {
     );
   }
 }
-
-/*
-ListView.builder(
-// Let the ListView know how many items it needs to build.
-itemCount: widget.child.vaccines.length,
-// Provide a builder function. This is where the magic happens.
-// Convert each item into a widget based on the type of item it is.
-itemBuilder: (context, index) {
-final item = widget.child.vaccines[index];
-return VaccineCard(item);
-},
-)*/
-
-// child: Column(
-// mainAxisAlignment: MainAxisAlignment.center,
-// children: <Widget>[
-// VaccineCard(Vaccine('Hepatitis B - 1', 0,1,'img/hepatitis_b.png',VaccineStatus.todo)),
-// VaccineCard(Vaccine('Hepatitis B - 2', 2,3,'img/hepatitis_b.png',VaccineStatus.todo),),
-// VaccineCard(Vaccine('Rotavirus', 7,8,'img/rotavirus.png',VaccineStatus.todo),),
-// VaccineCard(Vaccine('Influenza', 11,12,'img/influenza.png',VaccineStatus.later),),
-// ],
-// ),
