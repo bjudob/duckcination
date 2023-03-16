ac23b0b65f9837da098bac75df5699f3dfad55a0
Koncz Botond <konczbotondvagyok@gmail.com>
2023-03-17 01:52:36 +0200

    DuckProvider - 0.1 working

---
+++ lib/provider/duck_provider.dart
@@ -0,0 +1,14 @@
+import '../models/child.dart';
+
+class DuckProvider{
+  static List<Child> children = [
+    Child('Reki', DateTime(1998,9,26), Gender.girl, 6),
+    Child('Aliz', DateTime(2000,1,2), Gender.girl, 4),
+    Child('Boti', DateTime(1996,11,7), Gender.boy, 7),
+    Child('Lehel', DateTime(1999,9,17), Gender.boy, 3),
+  ];
+
+  List<Child> getChildren(){
+    return children;
+  }
+}
--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:duckcination/provider/duck_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,16 +6,19 @@ import '../../models/child.dart';
 import '../elements/child_card.dart';
 
 class DuckHome extends StatefulWidget {
-  const DuckHome({Key? key, required this.children}) : super(key: key);
-  final List<Child> children;
+  const DuckHome({Key? key}) : super(key: key);
 
   @override
   State<DuckHome> createState() => _DuckHomeState();
 }
 
 class _DuckHomeState extends State<DuckHome> {
+  final DuckProvider duckProvider = DuckProvider();
+  List<Child> children = [];
+
   @override
   Widget build(BuildContext context) {
+    children = duckProvider.getChildren();
     return Scaffold(
       backgroundColor: Colors.amberAccent.shade100,
       appBar: AppBar(
@@ -23,18 +27,18 @@ class _DuckHomeState extends State<DuckHome> {
       body: Center(
         child: SingleChildScrollView(
             child: Column(
-              children: [
-                SizedBox(
-                  height: 2000,
-                  child: ListView.builder(
-                      itemCount: widget.children.length,
-                      itemBuilder: (context, index) {
-                        final child = widget.children[index];
-                        return ChildCard(child: child);
-                      }),
-                )
-              ],
-            )),
+          children: [
+            SizedBox(
+              height: 2000,
+              child: ListView.builder(
+                  itemCount: children.length,
+                  itemBuilder: (context, index) {
+                    final child = children[index];
+                    return ChildCard(child: child);
+                  }),
+            )
+          ],
+        )),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
