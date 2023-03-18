35e2a3573df0f5d711c31d4cc49f188f2917dec2
Koncz Botond <konczbotondvagyok@gmail.com>
2023-03-18 18:47:30 +0200

    Duckhome <3 - more vaccines - scrollable

--- lib/ui/screens/duck_home.dart
+++ lib/ui/screens/duck_home.dart
@@ -35,126 +35,146 @@ class _DuckHomeState extends State<DuckHome> {
   Widget build(BuildContext context) {
     children = duckProvider.getChildren();
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                //colors: [Color.fromRGBO(255, 254, 254, 100),Color.fromRGBO(255, 240, 220, 100)]),
-                colors: [Color.fromRGBO(253, 200, 150, 100),Color.fromRGBO(255, 240, 220, 100)]),
-            borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(10, 65, 10, 20),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Image.asset(
-                          child.image,
-                          height: 100,
-                          width: 100,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 30),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              DropdownButton(
-                                  value: child.name,
-                                  items: children.map<DropdownMenuItem<String>>(
-                                      (Child child) {
-                                    return DropdownMenuItem<String>(
-                                      value: child.name,
-                                      child: Text(
-                                        child.name,
-                                        style: const TextStyle(
-                                          fontSize: 40,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    );
-                                  }).toList(),
-                                  onChanged: (String? name) {
-                                    selectChild(name!);
-                                  }),
-                              OutlinedButton(
-                                  onPressed: () {},
-                                  child:
-                                      const Text('Check upcoming appointment'))
-                            ],
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  //colors: [Color.fromRGBO(255, 254, 254, 100),Color.fromRGBO(255, 240, 220, 100)]),
+                  colors: [Color.fromRGBO(253, 200, 150, 100),Color.fromRGBO(255, 240, 220, 100)]),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 65, 10, 20),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Image.asset(
+                            child.image,
+                            height: 100,
+                            width: 100,
                           ),
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        BigButton(
-                            iconData: Icons.child_care_sharp,
-                            title: "Days until-Next Vaccine",
+                          Padding(
+                            padding: const EdgeInsets.only(left: 30),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                DropdownButton(
+                                    value: child.name,
+                                    items: children.map<DropdownMenuItem<String>>(
+                                        (Child child) {
+                                      return DropdownMenuItem<String>(
+                                        value: child.name,
+                                        child: Text(
+                                          child.name,
+                                          style: const TextStyle(
+                                            fontSize: 40,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                    onChanged: (String? name) {
+                                      selectChild(name!);
+                                    }),
+                                OutlinedButton(
+                                    onPressed: () {},
+                                    child:
+                                        const Text('Check upcoming appointment'))
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          BigButton(
+                              iconData: Icons.child_care_sharp,
+                              title: "Days until-Next Vaccine",
+                              target_screen: HistoryScreen(),
+                              option: 'days'),
+                          BigButton(
+                            iconData: Icons.calendar_today_outlined,
+                            title: "Vaccination-History",
                             target_screen: HistoryScreen(),
-                            option: 'days'),
-                        BigButton(
-                          iconData: Icons.calendar_today_outlined,
-                          title: "Vaccination-History",
-                          target_screen: HistoryScreen(),
-                          option: 'history',
-                        ),
-                        BigButton(
-                          iconData: Icons.perm_phone_msg_outlined,
-                          title: "    Call the    -Doctor",
-                          target_screen: HistoryScreen(),
-                          option: 'call',
-                        ),
-                      ],
-                    ),
-                  ],
+                            option: 'history',
+                          ),
+                          BigButton(
+                            iconData: Icons.perm_phone_msg_outlined,
+                            title: "    Call the    -Doctor",
+                            target_screen: HistoryScreen(),
+                            option: 'call',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                    color: Color.fromRGBO(252, 252, 252, 100),
-                    borderRadius: BorderRadius.all(Radius.circular(40))),
-                child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(10, 15,0,0),
-                      child: Text("Mandatory Vaccines", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ),
-                    SizedBox(
-                      height: 310,
-                      child: Container(
+                Container(
+                  decoration: const BoxDecoration(
+                      color: Color.fromRGBO(252, 252, 252, 100),
+                      borderRadius: BorderRadius.all(Radius.circular(40))),
+                  child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(10, 15,0,0),
+                        child: Text("Mandatory Vaccines", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      ),
+                      SizedBox(
+                        height: 310,
+                        child: Container(
+                          //color: Colors.grey[100],
+                          child: GridView.count(
+                              childAspectRatio: 0.8,
+                              crossAxisCount: 4,
+                              controller: ScrollController(keepScrollOffset: false),
+                              shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            children: child.vaccines.map((Vaccine vaccine) {
+                              return Container(
+                                height: 200.0,
+                                child: Center(
+                                  child: VaccineCard(vaccine)
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(10, 15,0,0),
+                        child: Text("Recommended Vaccines", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                      ),
+                      Container(
                         //color: Colors.grey[100],
                         child: GridView.count(
-                            childAspectRatio: 0.8,
-                            crossAxisCount: 4,
-                            controller: ScrollController(keepScrollOffset: false),
-                            shrinkWrap: true,
+                          childAspectRatio: 0.8,
+                          crossAxisCount: 4,
+                          controller: ScrollController(keepScrollOffset: false),
+                          shrinkWrap: true,
                           scrollDirection: Axis.vertical,
                           children: child.vaccines.map((Vaccine vaccine) {
                             return Container(
                               height: 200.0,
                               child: Center(
-                                child: VaccineCard(vaccine)
+                                  child: VaccineCard(vaccine)
                               ),
                             );
                           }).toList(),
                         ),
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(10, 15,0,0),
-                      child: Text("Recommended Vaccines", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ),
-                  ],
-                )),
-              ),
-            ],
+                    ],
+                  )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
