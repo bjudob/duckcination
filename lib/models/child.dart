dab1e18edec97ba2a0cb89eb9b5f4ed405634a2d
Koncz Botond <konczbotondvagyok@gmail.com>
2023-03-18 18:51:43 +0200

    Duckhome <3 - more vaccines - descriptions

--- lib/models/child.dart
+++ lib/models/child.dart
@@ -14,6 +14,7 @@ class Child{
   String name;
   DateTime birthDate;
   late List<Vaccine> vaccines;
+  late List<Vaccine> r_vaccines;
   Gender gender;
   int age;
   String image='img/boy.png';
@@ -23,31 +24,29 @@ class Child{
     if (gender == Gender.girl){
       image='img/girl.png';
     }
+    List<Description> descriptions=[
+      Description("Key vaccine facts", hepatitis_key_facts),
+      Description("Who should have the vaccine", hepatitis_key_facts),
+      Description("Safety and side effects", hepatitis_key_facts),
+      Description("Ingredients", hepatitis_key_facts),
+    ];
     vaccines=[
-      Vaccine('HepB', 0,1,'img/liver.png',VaccineStatus.done, [
-        Description("Key vaccine facts", hepatitis_key_facts),
-        Description("Who should have the vaccine", hepatitis_key_facts),
-        Description("Safety and side effects", hepatitis_key_facts),
-        Description("Ingredients", hepatitis_key_facts),
-      ]),
-      Vaccine('BCG', 2,3,'img/lung.png',VaccineStatus.done, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
-      Vaccine('Hexavalent', 7,8,'img/penta.png',VaccineStatus.todo, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
-      Vaccine('ROR', 11,12,'img/hand.png',VaccineStatus.done, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
-      Vaccine('Pneumococcal', 11,12,'img/ful.png',VaccineStatus.todo, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
-      Vaccine('DTPa-VPI', 2,3,'img/baci3.png',VaccineStatus.todo, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
-      Vaccine('dTpa', 7,8,'img/baci1.png',VaccineStatus.todo, [
-        Description("Key vaccine facts", hepatitis_key_facts)
-      ]),
+      Vaccine('HepB', 0,1,'img/liver.png',VaccineStatus.done, descriptions),
+      Vaccine('BCG', 2,3,'img/lung.png',VaccineStatus.done, descriptions),
+      Vaccine('Hexavalent', 7,8,'img/penta.png',VaccineStatus.todo, descriptions),
+      Vaccine('ROR', 11,12,'img/hand.png',VaccineStatus.done, descriptions),
+      Vaccine('Pneumococcal', 11,12,'img/ful.png',VaccineStatus.todo, descriptions),
+      Vaccine('DTPa-VPI', 2,3,'img/baci3.png',VaccineStatus.todo, descriptions),
+      Vaccine('dTpa', 7,8,'img/baci1.png',VaccineStatus.todo, descriptions),
+    ];
+    r_vaccines=[
+      Vaccine('HepB', 0,1,'img/liver.png',VaccineStatus.done, descriptions),
+      Vaccine('BCG', 2,3,'img/lung.png',VaccineStatus.done, descriptions),
+      Vaccine('Hexavalent', 7,8,'img/penta.png',VaccineStatus.todo, descriptions),
+      Vaccine('ROR', 11,12,'img/hand.png',VaccineStatus.done, descriptions),
+      Vaccine('Pneumococcal', 11,12,'img/ful.png',VaccineStatus.todo, descriptions),
+      Vaccine('DTPa-VPI', 2,3,'img/baci3.png',VaccineStatus.todo, descriptions),
+      Vaccine('dTpa', 7,8,'img/baci1.png',VaccineStatus.todo, descriptions),
     ];
   }
 }
